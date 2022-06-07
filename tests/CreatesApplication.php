156a12ca709898c18eb131b4cf0d006123a9355d
diego lozano <jferdi24@gmail.com>
2022-06-06 22:02:07 -0500

    :bug: test: fix test vacancy store

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -2,6 +2,8 @@
 
 namespace Tests;
 
+use Database\Seeders\SkillSeeder;
+use Database\Seeders\TypeWorkSeeder;
 use Illuminate\Contracts\Console\Kernel;
 
 trait CreatesApplication
@@ -16,6 +18,9 @@ trait CreatesApplication
         $app = require __DIR__.'/../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
+
+        app(SkillSeeder::class)->run();
+        app(TypeWorkSeeder::class)->run();
 
         return $app;
     }
--- tests/Feature/Http/Controllers/Api/v1/VacancyControllerTest.php
+++ tests/Feature/Http/Controllers/Api/v1/VacancyControllerTest.php
@@ -8,6 +8,8 @@ use Tests\TestCase;
 
 class VacancyControllerTest extends TestCase
 {
+    use RefreshDatabase, WithFaker;
+
     /**
      * A basic feature test example.
      *
@@ -15,36 +17,50 @@ class VacancyControllerTest extends TestCase
      */
     public function test_store()
     {
-        // $this->withoutExceptionHandling();
-        $response = $this->json('POST', '/v1/vacancies', [
-            'name',
-            'postulation_deadline',
-            'description',
-            'status',
-            'salary',
-            'company_id',
-            'typeWork',
-            'job_location',
-            'skills',
-            'hours_per_week',
-            'minimum_experience',
+        /**
+         * "name": "Frontend Developer",
+         * "user_id": "uuid of auth0",
+         * "postulation_deadline": "2022-05-01 13:50:45",
+         * "description": "Details contract.",
+         * "status": 1,
+         * "salary": 1500,
+         * "company_id": 1,
+         * "typeWork": 1,
+         * "job_location": "cra 23 # 11 -62 Bucaramanga Santander",
+         * "skills": "React, Php, Node, Angular",
+         * "hours_per_week": 48,
+         * "minimum_experience": 5         */
+        $response = $this->post('api/v1/vacancies', [
+            'name' => $this->faker->name(),
+            'postulation_deadline' => '2022-05-01 13:50:45',
+            'description' => $this->faker->paragraph(),
+            'status' => 1,
+            'salary' => 1000,
+            'company_id' => 1,
+            'typeWork' => 1,
+            'job_location' => 'address',
+            'skills' => 'React, Php, Node, Angular',
+            'hours_per_week' => 40,
+            'minimum_experience' => 1,
+            'user_id' => 'user-id'
         ]);
 
         $response->assertJsonStructure([
-            'id',
-            'name',
-            'postulation_deadline',
-            'description',
-            'status',
-            'salary',
-            'company_id',
-            'typeWork',
-            'job_location',
-            'skills',
-            'hours_per_week',
-            'minimum_experience',
-            'created_at',
-            'update_at',
+            "data" => [
+                'id',
+                'name',
+                'postulation_deadline',
+                'description',
+                'status',
+                'salary',
+                'company_id',
+                'typeWork',
+                'job_location',
+                'skills',
+                'hours_per_week',
+                'minimum_experience',
+                'created_at'
+            ],
         ]);
     }
 }
